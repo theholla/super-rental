c95c137200b8f01657027deb3041c08eb0fdc7c5
the holla <holla@bikefarm.org>
2015-09-28 23:25:02 -0700

    Add behavior for updating rentals

--- app/components/update-rental.js
+++ app/components/update-rental.js
@@ -1,13 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  addNewRental: false,
+  updateRentalForm: false,
   actions: {
-    rentalFormShow() {
-      this.set('addNewRental', true);
+    updateRentalForm() {
+      this.set('updateRentalForm', true);
     },
-
-    save1() {
+    update(rental) {
       var params = {
         owner: this.get('owner'),
         city: this.get('city'),
@@ -15,9 +14,8 @@ export default Ember.Component.extend({
         image: this.get('image'),
         bedrooms: this.get('bedrooms'),
       };
-      this.set('addNewRental', false),
-      this.sendAction('save2', params); //Expected an assignment or function call and instead saw an expression.
-
+      this.set('updateRentalForm', false);
+      this.sendAction('update', rental, params);
     }
   }
 });
